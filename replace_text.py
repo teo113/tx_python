2c9004c71e7368e513d81a3c727ae539e86daf2b
teo113 <faull.theo@gmail.com>
2021-03-22 22:03:48 +0000

    updated gitignore and replace text

---
+++ replace_text.py
@@ -0,0 +1,17 @@
+# author: github.com/teo113
+# desc: This script finds text in a text based input file, replaces the text, creates a new output file.
+
+in_file = open(fr'D:\path_to_file\input.txt', 'r')
+out_file = open(fr'D:\path_to_file\output.txt', 'r')
+
+find = ('\\', 'this word', 'hello')
+replace = ('/', 'that word', 'goodbye')
+
+for line in in_file:
+    for check, rep in zip(find, replace):
+        line = line.replace(check, rep)
+    out_file.write(line)
+in_file.close()
+out_file.close()
+
+# repeat for other files
