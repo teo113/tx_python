5f0adb58218726b10547145d67ff219d8a12036d
teo113 <faull.theo@gmail.com>
2021-03-23 00:23:50 +0000

    added 2 empty db conn scripts

---
+++ shakey_encryption.py
@@ -0,0 +1,2 @@
+# author: github.com/teo113 & github.com/waggett96
+# desc: This script encrypts a config file for use within a db connector
---
+++ tx_db_connector.py
@@ -0,0 +1,2 @@
+# author: github.com/teo113 & github.com/bm13563
+# desc: This script acts as a secure database connector file for connecting to PostgreSQL via psycopg2 and gdal
