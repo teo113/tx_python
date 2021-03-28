2c3e7c5856f01675ce1cf020062afb4347e20c88
teo113 <faull.theo@gmail.com>
2021-03-28 18:03:05 +0100

    updated syntax
    Syntax update for python version > 3.8. Replacing is with ==.
    
    Removed import time as not used.

--- date_stamper.py
+++ date_stamper.py
@@ -1,7 +1,6 @@
 # author: github.com/teo113
 # desc: This script prints various date and time formats to the console
 
-import time
 from datetime import datetime
 
 # YYYYMMDD
@@ -17,25 +16,25 @@ monyear = datetime.today().strftime('%b%y')
 print(monyear.upper())
 
 # pretty date
-if datetime.today().day is 1:
+if datetime.today().day == 1:
     pretty_date = datetime.today().strftime('%dst %B %Y')
     print(pretty_date)
-elif datetime.today().day is 2:
+elif datetime.today().day == 2:
     pretty_date = datetime.today().strftime('%dnd %B %Y')
     print(pretty_date)
-elif datetime.today().day is 3:
+elif datetime.today().day == 3:
     pretty_date = datetime.today().strftime('%drd %B %Y')
     print(pretty_date)
-elif datetime.today().day is 21:
+elif datetime.today().day == 21:
     pretty_date = datetime.today().strftime('%dst %B %Y')
     print(pretty_date)
-elif datetime.today().day is 22:
+elif datetime.today().day == 22:
     pretty_date = datetime.today().strftime('%dnd %B %Y')
     print(pretty_date)
-elif datetime.today().day is 23:
+elif datetime.today().day == 23:
     pretty_date = datetime.today().strftime('%drd %B %Y')
     print(pretty_date)
-elif datetime.today().day is 31:
+elif datetime.today().day == 31:
     pretty_date = datetime.today().strftime('%dst %B %Y')
     print(pretty_date)
 else:
