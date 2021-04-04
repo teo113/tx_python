d90ab8e0116868f13ec281cdb27ea5753d7ca91d
teo113 <faull.theo@gmail.com>
2021-04-04 22:28:29 +0100

    added pandas method

--- csv_as_list.py
+++ csv_as_list.py
@@ -2,6 +2,7 @@
 # desc: This script reads in a csv file/column as a list.
 
 import csv
+import pandas as pd
 
 csv_file = r'data/list.csv'
 
@@ -15,13 +16,16 @@ print(data)
 
 
 # pass each value found in a chosen column as a list
-col_list = []
+col2_list = []
 with open(f'{csv_file}', newline='') as f:
     reader = csv.reader(f)
     next(reader, None)  # skip header row
     for row in csv.reader(f):
-        col_list.append(row[1])
+        col2_list.append(row[1])
 
-print(col_list)
+print(col2_list)
 
 # use pandas to pass a chosen column as a list
+df = pd.read_csv(f'{csv_file}')
+locations = df.column2.to_list()
+print(locations)
